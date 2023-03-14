f11c6c26ac71404472ca7f6cec15fac1a3b0d228
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-03-14 19:57:09 +0300

    Complete assignment

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,42 @@
+class SpicesController < ApplicationController
+    wrap_parameters format:[]
+    #GET /spices: return an array of all spices
+    def index
+        spices = Spice.all
+        render json: spices
+    end
+
+    #POST /spices: create a new spice
+    def create
+        spice = Spice.create(spice_params)
+        render json: spice, status: :created
+    end
+
+    #PATCH /spices/:id: update an existing spice
+    def update
+        spice = Spice.find_by(id:params[:id])
+        if spice
+            spice.update(spice_params)
+            render json: spice
+        else
+            render json: {error:"Spice not found"}, status: :not_found
+        end
+    end
+
+    #DELETE /spices/:id: delete an existing spice
+    def destroy
+        spice = Spice.find_by(id:params[:id])
+        if spice
+            spice.destroy
+            head :no_content
+        else
+            render json: {error:"Spice not found"}, status: :not_found
+        end
+    end
+
+    private
+
+    def spice_params
+        params.permit(:title, :image, :description, :notes, :rating)
+    end
+end
